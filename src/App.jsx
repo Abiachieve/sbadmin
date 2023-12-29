dbd2c168bee99b01d1ebfa1d366338da0a6555c9
Abiachieve <abinaya.achieve123@gmail.com>
2023-12-29 17:14:50 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import Sidebar from "./components/Sidebar"
 import Dashboard from "./components/Dashboard"
-
+import Create from "./components/Create"
+import Edit from "./components/Edit"
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { useState } from "react"
 function App() {
- 
+ let[data,setData]= useState([
+{
+   name:"Vishaka",
+   username:"vis123",
+   email:"vishaka@gmail.com",
+   mobile:8977645431,
+   batch:"B50WDT",
+
+},
+{
+  name:"Sim",
+  username:"sim123",
+  email:"sim@gmail.com",
+  mobile:9977645431,
+  batch:"B51WDT",
+}
+
+ ])
 
   return <>
    <div id="wrapper">
+    <BrowserRouter>
     <Sidebar/>
-    <Dashboard/>
+    <Routes>
+    <Route path='/dashboard' element={<Dashboard data={data} setData={setData}/> }/>
+    <Route path='/create' element={<Create/>}/>
+    <Route path='/edit' element={<Edit/>}/>
+    </Routes>
+    </BrowserRouter>
+   
    </div>
   </>
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Tile from './Tile'
-function Dashboard() {
-    
-    let data=
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+function Dashboard({data,setData}) {
+ console.log(data)
+    let dashboardData=
     [{
         color:"primary",
         icon:"fa-calendar",
@@ -33,6 +35,11 @@ function Dashboard() {
 
 
 ]
+let handleDelete = (index)=>{
+    let newArray= [...data]
+    newArray.splice(index,1)
+    setData(newArray)
+}
   return <>
   <div className="container-fluid">
   <div className="d-sm-flex align-items-center justify-content-between mb-4">
@@ -43,7 +50,7 @@ function Dashboard() {
                     <div className="row">
 
                        {
-                        data.map((e,i)=>{
+                        dashboardData.map((e,i)=>{
                             return<Tile color={e.color}
                             icon={e.icon}
                             title={e.title}
@@ -55,6 +62,42 @@ function Dashboard() {
                         })
                        }
                     </div>
+                    <div className="row">
+                        <Table striped bordered hover>
+                        <thead>
+        <tr>
+          <th>SNo</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Mobile</th>
+          <th>Batch</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+
+       {
+        data.map((e,i)=>{
+            return<tr key={i}>
+                <td>{i+1}</td>
+                <td>{e.name}</td>
+                <td>{e.username}</td>
+                <td>{e.email}</td>
+                <td>{e.mobile}</td>
+                <td>{e.batch}</td>
+                <td> <Button variant='primary'>Edit</Button>
+                &nbsp;&nbsp;
+                <Button variant='danger' onClick={()=>handleDelete(i)}>Delete</Button>
+                </td>
+            </tr>
+        })
+       }
+       
+       
+      </tbody>
+      </Table>
+     </div>
   </div>
   </>
 }
